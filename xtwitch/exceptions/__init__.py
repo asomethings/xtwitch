6d92e5851d5f9b9e819a9d6799d9dde7d218095e
asomethings <asomethings@kakao.com>
2019-12-12 23:37:08 +0900

    ADD created exceptions to __init__.py

---
+++ xtwitch/exceptions/__init__.py
@@ -0,0 +1,4 @@
+from xtwitch.exceptions.base import TwitchException
+from xtwitch.exceptions.client import ClientException, ClientImproperlyConfigured
+
+__all__ = ['TwitchException', 'ClientException', 'ClientImproperlyConfigured']
