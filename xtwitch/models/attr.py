3d07e5e8fabf7bb030610f6e7b50ca80b213ceb5
asomethings <asomethings@kakao.com>
2019-12-22 15:04:20 +0900

    RENAME JsonParser to Attrs with support of attr.s

--- xtwitch/models/attr.py
+++ xtwitch/models/attr.py
@@ -1,19 +1,40 @@
+from __future__ import annotations
+
+import inspect
 from typing import Callable
 
 import attr
 
-from xtwitch.models.base import JsonParser
-
 
 def optional(converter: Callable, *args, **kwargs):
-    if converter is issubclass(converter, JsonParser):
+    if inspect.isclass(converter) and issubclass(converter, Attrs):
         converter = converter.from_json
 
     return attr.ib(converter=attr.converters.optional(converter), default=None, *args, **kwargs)
 
 
-def attrs(*args, **kwargs):
-    return attr.s(kw_only=True, auto_attribs=True, *args, **kwargs)
+class AttrsMetaClass(type):
+
+    def __new__(mcs, *args, **kwargs):
+        obj = super(AttrsMetaClass, mcs).__new__(mcs, *args, **kwargs)
+        return attr.s(kw_only=True, auto_attribs=True)(obj)
 
 
-s = attrs
+class Attrs(metaclass=AttrsMetaClass):
+
+    @classmethod
+    def from_json(cls, data: dict) -> Attrs:
+        if not attr.has(cls):
+            raise Exception
+
+        filtered_dict = dict()
+        for k, v in data.items():
+            if k not in cls.__annotations__.keys():
+                continue
+
+            filtered_dict.update({k: v})
+
+        return cls(**filtered_dict)
+
+
+S = Attrs
--- xtwitch/models/base.py
+++ xtwitch/models/base.py
@@ -1,28 +1,10 @@
 from __future__ import annotations
 
-import attr
-
+from xtwitch.models import attr
 from xtwitch.models.manager import Manager
 
 
-class JsonParser:
-
-    @classmethod
-    def from_json(cls, data: dict) -> JsonParser:
-        if not attr.has(cls):
-            raise Exception
-
-        filtered_dict = dict()
-        for k, v in data.items():
-            if k not in cls.__annotations__.keys():
-                continue
-
-            filtered_dict.update({k: v})
-
-        return cls(**filtered_dict)
-
-
-class ModelMetaClass(type):
+class ModelMetaClass(attr.AttrsMetaClass):
 
     def __new__(mcs, name, bases, attrs, **kwargs):
         if 'manager' not in attrs:
@@ -32,8 +14,8 @@ class ModelMetaClass(type):
             raise Exception
 
         obj = super(ModelMetaClass, mcs).__new__(mcs, name, bases, attrs)
-        return attr.s(kw_only=True)(obj)
+        return obj
 
 
-class Model(JsonParser, metaclass=ModelMetaClass):
+class Model(attr.S, metaclass=ModelMetaClass):
     manager = Manager()
