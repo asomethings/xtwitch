e53dc263b7163f793ea972c9b8afd3721fd836d4
asomethings <asomethings@kakao.com>
2019-12-22 14:29:46 +0900

    REFACTOR ModelMetaClass to check manager variable

--- xtwitch/models/base.py
+++ xtwitch/models/base.py
@@ -6,32 +6,15 @@ from xtwitch.models.manager import Manager
 class ModelMetaClass(type):
 
     def __new__(mcs, name, bases, attrs, **kwargs):
-        new_object: ModelMetaClass = super(ModelMetaClass, mcs).__new__(mcs, name, bases, attrs)
-        new_object._setup_manager()
-        return new_object
+        if 'manager' not in attrs:
+            raise Exception
 
-    def _setup_manager(cls) -> None:
-        manager_name = f'{cls.__name__}{Manager.__name__}'
-        manager_methods = cls._get_methods()
-        manager = type(manager_name, (Manager,), manager_methods)
-        setattr(cls, 'manager', manager())
+        if not isinstance(attrs['manager'], Manager):
+            raise Exception
 
-    def _get_methods(cls) -> dict:
-        methods = dict()
-        members = inspect.getmembers(cls, predicate=inspect.isfunction)
-        for name, method in members:
-            is_private = getattr(method, 'is_private', None)
-            if is_private:
-                continue
-
-            if is_private is None and name.startswith('_'):
-                continue
-
-            methods[name] = method
-
-        return methods
+        obj = super(ModelMetaClass, mcs).__new__(mcs, name, bases, attrs)
+        return attr.s(kw_only=True)(obj)
 
 
-class Model(metaclass=ModelMetaClass):
-    pass
-
+class Model(JsonParser, metaclass=ModelMetaClass):
+    manager = Manager()
